abb2c549d24fbb66e0f1481b53efff5c50c8544c
parkmynggun <psk88009955@naver.com>
2025-09-16 18:56:55 +0900

    조이스틱 코드완성2

--- soc/_ide/workspace_journal.py
+++ soc/_ide/workspace_journal.py
@@ -18,3 +18,15 @@ status = platform.build()
 
 comp.build()
 
+client.delete_component(name="app_joystick")
+
+platform = client.create_platform_component(name = "platform_jy",hw_design = "$COMPONENT_LOCATION/../../robo-digger-project-work/soc_pwm_led_wrapper_adc_uart_btn_lcd.xsa",os = "standalone",cpu = "microblaze_riscv_0",domain_name = "standalone_microblaze_riscv_0")
+
+comp = client.create_app_component(name="app_jy",platform = "$COMPONENT_LOCATION/../platform_jy/export/platform_jy/platform_jy.xpfm",domain = "standalone_microblaze_riscv_0",template = "hello_world")
+
+platform = client.get_component(name="platform_jy")
+status = platform.build()
+
+comp = client.get_component(name="app_jy")
+comp.build()
+
---
+++ soc/platform_jy/microblaze_riscv_0/standalone_microblaze_riscv_0/bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,11 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "/home/user11/robo-digger-project/soc/platform_jy/microblaze_riscv_0/standalone_microblaze_riscv_0/bsp/include/sleep.h"
+  "/home/user11/robo-digger-project/soc/platform_jy/microblaze_riscv_0/standalone_microblaze_riscv_0/bsp/include/xiltimer.h"
+  "/home/user11/robo-digger-project/soc/platform_jy/microblaze_riscv_0/standalone_microblaze_riscv_0/bsp/include/xtimer_config.h"
+  "/home/user11/robo-digger-project/soc/platform_jy/microblaze_riscv_0/standalone_microblaze_riscv_0/bsp/lib/libxiltimer.a"
+  )
+endif()
